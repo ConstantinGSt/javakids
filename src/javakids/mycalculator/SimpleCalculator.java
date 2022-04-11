8b7e97357b52ffd8a7c164371aff651e1a7cdac9
DinSV <dinscalin.ks@gmail.com>
2022-04-11 17:56:43 +0700

    add class Calc

---
+++ src/javakids/mycalculator/SimpleCalculator.java
@@ -0,0 +1,15 @@
+package javakids.mycalculator;
+
+import javax.swing.*;
+import java.awt.FlowLayout;
+
+public class SimpleCalculator {
+	public static void main(String[] args) {
+		
+		JPanel windowContent = new JPanel();
+		FlowLayout fl = new FlowLayout();
+		windowContent.setLayout(fl);
+		
+	}
+
+}
