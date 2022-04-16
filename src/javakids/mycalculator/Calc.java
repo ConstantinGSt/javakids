86c233f611f81aac79315027d27f581fa4cc3ef3
DinSV <dinscalin.ks@gmail.com>
2022-04-16 13:11:31 +0700

    add button[]

--- src/javakids/mycalculator/Calc.java
+++ src/javakids/mycalculator/Calc.java
@@ -11,16 +11,16 @@ public class Calc {
 	JPanel windowContent;
 	JTextField displayField;
 	JButton [] button = new JButton[10];
-//	JButton button0;
-//	JButton button1;
-//	JButton button2;
-//	JButton button3;
-//	JButton button4;
-//	JButton button5;
-//	JButton button6;
-//	JButton button7;
-//	JButton button8;
-//	JButton button9;
+	JButton button0;
+	JButton button1;
+	JButton button2;
+	JButton button3;
+	JButton button4;
+	JButton button5;
+	JButton button6;
+	JButton button7;
+	JButton button8;
+	JButton button9;
 	JButton buttonPoint;
 	JButton buttonEqual;
 	JButton buttonPlas;
@@ -46,21 +46,29 @@ public class Calc {
 		
 		// create button use construct
 		// JButton - input text Button as option
-//		int i = 0;
-//		while(i<10) {
-//			button[i] = new JButton("i");
-//			i++;
-//		}
 		
-//		button1=new JButton("1");
-//		button2=new JButton("2");
-//		button3=new JButton("3");
-//		button4=new JButton("4");
-//		button5=new JButton("5");
-//		button6=new JButton("6");
-//		button7=new JButton("7");
-//		button8=new JButton("8");
-//		button9=new JButton("9");
+		
+		button1=new JButton("1");
+		button2=new JButton("2");
+		button3=new JButton("3");
+		button4=new JButton("4");
+		button5=new JButton("5");
+		button6=new JButton("6");
+		button7=new JButton("7");
+		button8=new JButton("8");
+		button9=new JButton("9");
+		button0 = new JButton("0");
+		button[0] = button1;
+		button[1] = button2;
+		button[2] = button3;
+		button[3] = button4;
+		button[4] = button5;
+		button[5] = button6;
+		button[6] = button7;
+		button[7] = button8;
+		button[8] = button9;
+		button[9] = button0;		
+		
 		buttonPoint = new JButton(".");
 		buttonEqual=new JButton("=");
 		buttonPlas = new JButton("+");
@@ -75,16 +83,17 @@ public class Calc {
 		GridLayout gl = new GridLayout(4, 3);
 		p1.setLayout(gl);
 		
-		p2 = new JPanel();
-		GridLayout gl2 = new GridLayout(4, 1);
-		p2.setLayout(gl2);
-		
 		int i = 0;
-		while(i<10) {
-			button[i] = new JButton();
+		while(i < 10) {
 			p1.add(button[i]);
 			i++;
 		}
+		
+		p2 = new JPanel();
+		GridLayout gl2 = new GridLayout(4, 1);
+		p2.setLayout(gl2);
+	
+		
 		// add buttons to panel 
 //		p1.add(button[i]);
 //		p1.add(button2);
